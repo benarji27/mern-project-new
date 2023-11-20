251755efc2dd11845be06b4421a41c62bd657857
benarji27 <bennu7701@gmail.com>
2023-11-20 16:15:44 +0530

    vercel update

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,8 @@ const dotenv = require("dotenv")
 const app = express()
 const Routes = require("./routes/route.js")
 
+const cors = require('cors');
+
 const PORT = process.env.PORT || 5000
 
 dotenv.config();
@@ -14,7 +16,13 @@ dotenv.config();
 // app.use(bodyParser.urlencoded({ limit: '10mb', extended: true }))
 
 app.use(express.json({ limit: '10mb' }))
-app.use(cors())
+app.use(cors(
+    {
+        origin: ["https://mern-project-b62g3u1nn-benarjis-projects.vercel.app/"],
+        methods: ["POST", "GET"],
+        crerdentials:true
+    }
+))
 
 mongoose
     .connect(process.env.MONGO_URL, {
